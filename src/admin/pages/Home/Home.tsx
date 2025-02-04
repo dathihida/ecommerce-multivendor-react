5d025b5fdb44b894fd8d5b5ab47f72aca985e25a
Phùng Văn Tiến Đạt <tiendat060709@gmail.com>
2025-02-04 21:20:35 +0700

    feature: implement shopByCategoy

--- src/admin/pages/Home/Home.tsx
+++ src/admin/pages/Home/Home.tsx
@@ -3,6 +3,8 @@ import ElectricCategory from './ElectricCategory/ElectricCategory'
 import CategoryGrid from './CategoryGrid/CategoryGrid'
 import Deal from './Deal/Deal'
 import ShopByCategory from './ShopByCategory/ShopByCategory'
+import { Button } from '@mui/material'
+import { Storefront, StoreSharp } from '@mui/icons-material'
 
 const Home = () => {
   return (
@@ -10,8 +12,35 @@ const Home = () => {
         <div className='space-y-5 lg:space-y-10 relative pb-20'>
             <ElectricCategory/>
             <CategoryGrid/>
-            <Deal/>
-            <ShopByCategory/>
+
+            <section className='pt-20'>
+              <h1 className='text-lg lg:text-4xl font-bold text-primary-colors pb-5 lg:px-20 text-center'>
+                TODAY'S DEAL
+              </h1>
+              <Deal/>
+            </section>
+
+            <section className='pt-20'>
+              <h1 className='text-lg lg:text-4xl font-bold text-primary-colors pb-5 lg:px-20 text-center'>SHOP BY CATEGORY</h1>
+              <ShopByCategory/>
+            </section>
+            
+            <section className='mt-20 lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
+              <img className='w-full h-full'
+                src="https://as1.ftcdn.net/v2/jpg/03/20/68/66/1000_F_320686681_Ur6vdYQgDC9WiijiVfxlRyQffxOgfeFz.jpg" alt="" />
+              <div className='absolute top-1/2 left-3/4 
+                transform -translate-x-1/2 -translate-y-1/2 font-semibold lg:text-4xl space-y-3 text-center'>
+                <h1 className='text-2xl lg:text-4xl font-bold text-white pb-5'>Sell your product</h1>
+                <p className='text-lg md:text-2xl'>With<span className='logo'> DatShop</span></p>
+                <div className='pt-6 flex justify-center'>
+                  <Button startIcon={<Storefront/>} variant='contained' size='large'>
+                    Become Seller
+                  </Button>
+                </div>
+              </div>
+              
+              
+            </section>
         </div>
     </>
   )
--- src/admin/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
+++ src/admin/pages/Home/ShopByCategory/ShopByCategoryCard.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './ShopByCategory.css'
 const ShopByCategoryCard = () => {
   return (
-    <div>ShopByCategoryCard</div>
+    <div className="flex gap-2 flex-col justify-center items-center group cursor-pointer">
+        <div className="custome-border w-[150px] h-[150px] lg:w-[249px] lg:h-[249px] rounded-full bg-primary-colors flex justify-center items-center overflow-hidden">
+            <img
+                className="rounded-full group-hover:scale-95 transition-transform duration-700 object-cover object-top w-full h-full"
+                src="https://zshop.vn/images/thumbnails/755/556/detailed/51/iphone-12-select-2021_ntej-7k.jpg"
+                alt="iPhone 12"
+            />
+        </div>
+    <h1 className="text-center">Iphone 12</h1>
+</div>
+
   )
 }
 
